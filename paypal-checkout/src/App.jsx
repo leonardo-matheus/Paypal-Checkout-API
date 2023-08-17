75d9dbcea404301945c78666afa8f426caf7de97
Leonardo Silva <leonardo@d3f4lt.com.br>
2023-08-17 12:55:18 -0300

    Init and Config index file

---
+++ paypal-checkout/src/App.jsx
@@ -0,0 +1,146 @@
+import './styles.scss';
+
+import PageHeader from './layout/PageHeader';
+import PageTitle from './layout/PageTitle';
+import Summary from './Summary';
+import TableRow from './TableRow';
+import { useEffect, useState } from 'react';
+
+import { api } from './provider';
+import axios from 'axios';
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+  const productObject = {
+    name: 'produto',
+    category: 'categoria',
+    price: randomNumber(90, 1200),
+    quantity: 1,
+  };
+
+  const fetchData = () => {
+    console.log('fetch data');
+    api.get('/cart').then((response) => setCart(response.data));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleAddItem = () => {
+    // Insert
+
+    console.log('disparou handleAddItem');
+
+    api.post('/cart', productObject).then((response) => {
+      console.log(response);
+      fetchData();
+    });
+  };
+
+  const handleRemoveItem = (item) => {
+    // Remove
+
+    console.log('disparou handleRemoveItem');
+    console.log({ item });
+
+    api.delete(`/cart/${item._id}`).then((response) => {
+      console.log(response);
+      fetchData();
+    });
+  };
+
+  const handleUpdateItem = (item, action) => {
+    // Quantity
+
+    let newQuantity = item.quantity;
+
+    if (action === 'decrease') {
+      if (newQuantity === 1) {
+        return;
+      }
+      newQuantity -= 1;
+    }
+    if (action === 'increase') {
+      newQuantity += 1;
+    }
+
+    const newData = { ...item, quantity: newQuantity };
+    delete newData._id;
+
+    console.log({ newData });
+    api.put(`/cart/${item._id}`, newData).then((response) => {
+      console.log({ response });
+      fetchData();
+    });
+  };
+
+  const getTotal = () => {
+    let sum = 0;
+
+    for (let item of cart) {
+      sum += item.price * item.quantity;
+    }
+
+    return sum;
+  };
+
+  const cartTotal = getTotal();
+
+  return (
+    <>
+      <PageHeader />
+      <main>
+        <PageTitle data={'Seu carrinho'} />
+        <div className='content'>
+          <section>
+            <button
+              onClick={handleAddItem}
+              style={{ padding: '5px 10px', marginBottom: 15 }}
+            >
+              add to cart
+            </button>
+            <table>
+              <thead>
+                <tr>
+                  <th>Produto</th>
+                  <th>Preço</th>
+                  <th>Quantidade</th>
+                  <th>Total</th>
+                  <th>-</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cart.map((item) => (
+                  <TableRow
+                    key={item._id}
+                    data={item}
+                    handleRemoveItem={handleRemoveItem}
+                    handleUpdateItem={handleUpdateItem}
+                  />
+                ))}
+                {cart.length === 0 && (
+                  <tr>
+                    <td colSpan='5' style={{ textAlign: 'center' }}>
+                      <b>Carrinho de compras vazio.</b>
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </section>
+          <aside>
+            <Summary total={cartTotal} />
+          </aside>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
